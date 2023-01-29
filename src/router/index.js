1874d485552275944ed30f41749154163f231956
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2023-01-29 13:32:06 +0100

    Routage

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/contact', name: 'contact', component: () => import('../views/ContactView.vue') },
-
+    { path: '/',                      name: 'home',                     component: () => import('../views/HomeView.vue') },
+    { path: '/contact',               name: 'contact',                  component: () => import('../views/ContactView.vue') },
+    { path: '/valeurs',               name: 'valeurs',                  component: () => import('../views/ValuesView.vue') },
+    { path: '/ciluzio',               name: 'ciluzio',                  component: () => import('../views/CiluzioView.vue') },
+    { path: '/inspirations',          name: 'inspirations',             component: () => import('../views/InspirationView.vue') },
+    { path: '/panier',                name: 'panier',                   component: () => import('../views/CartView.vue') },
+    { path: '/compte',                name: 'compte',                   component: () => import('../views/AccountView.vue') },
+    { path: '/:pathMatch(.*)*',       name: 'page-introuvable',         component: () => import('../views/NotFindView.vue') },
   ]
 })
 
