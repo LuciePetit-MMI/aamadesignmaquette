068f3a064734cd9c12bcc9ee7a428cbe77c43cf4
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2023-01-29 13:33:40 +0100

    Header + Footer + HomeView + buttons component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,3 @@
-<script>
-export default({
-  data() {
-    return{
-      openNav: false,
-    }
-  }
-})
-</script>
-
 <template>
   <div class="fixed z-50 backdrop-blur">
     <div class="menu relative flex flex-row justify-between items-center w-screen px-5 py-2">
@@ -23,35 +13,25 @@ export default({
         <nav :class="{'block': openNav, 'hidden': !openNav}" class="absolute top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-white text-center z-10 laptop:static laptop:max-w-fit laptop:block laptop:h-full laptop:bg-[transparent]">
             <ul class="flex flex-col laptop:flex-row">
                 <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/" class="font-saotorpes text-marine text-2xl">Accueil</router-link></li>
-                <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/" class="font-saotorpes text-marine text-2xl">Ciluzio</router-link></li>
-                <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/" class="font-saotorpes text-marine text-2xl">Panier</router-link></li>
-                <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/" class="font-saotorpes text-marine text-2xl">Mon compte</router-link></li>
+                <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/ciluzio" class="font-saotorpes text-marine text-2xl">Ciluzio</router-link></li>
+                <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/panier" class="font-saotorpes text-marine text-2xl">Panier</router-link></li>
+                <li class="mb-8 laptop:mb-0 laptop:mr-8"><router-link to="/compte" class="font-saotorpes text-marine text-2xl">Mon compte</router-link></li>
             </ul>
         </nav>
     </div>    
 </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+<script>
+export default({
+  data() {
+    return{
+      openNav: false,
+    }
   }
-}
+})
+</script>
+
+<style scoped>
+
 </style>
