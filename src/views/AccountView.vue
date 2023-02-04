f6b7b29a06f7a49cb868893b341a95cc2ee08129
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2023-02-04 11:00:49 +0100

    Signs view / fonctionnement de la section compte avec formulaire de connection

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,7 +1,41 @@
 <template>
-    <div>
-        <main class="mt-24 px-5 laptop:px-20">
+    <div class="px-5 laptop:px-20" :class="{'bg-transparant h-fit w-fit': step, 'bg-beige h-screen w-screen': !step}">
+        <div v-if="!step" class="signs pt-24 mb-12 px-5 laptop:px-20">
             <div>
+                <form class="laptop:grid laptop:grid-cols-2 laptop:gap-10" @submit="onSubmit">
+                    <div v-if="connection==='signup'" action="" class="flex flex-col">
+                        <h1 class="h1">Connexion</h1>
+                        <p>Vous n'avez pas de compte ? <span class="underline" @click="connection='signin'">Inscrivez-vous</span></p>        
+                        <label for="" class="text-marine text-xl">Email</label>
+                        <input type="email" placeholder="Email" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <label for="" class="text-marine text-xl">Mot de passe</label>
+                        <input type="password" placeholder="********" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <input @click="connectAccount" type="submit" value="Se connecter" class="button bg-marine">
+                    </div>
+                    <div v-if="connection==='signin'" action="" class="flex flex-col">
+                        <h1 class="h1">Inscription</h1>
+                        <p>Vous avez déjà un compte ? <span class="underline" @click="connection='signup'">Connectez-vous</span></p>    
+                        <label for="" class="text-marine text-xl">Nom</label>
+                        <input type="text" placeholder="Nom" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <label for="" class="text-marine text-xl">Prénom</label>
+                        <input type="text" placeholder="Prénom" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <label for="" class="text-marine text-xl">Email</label>
+                        <input type="email" placeholder="Email" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <label for="" class="text-marine text-xl">Mot de passe</label>
+                        <input type="password" placeholder="********" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <label for="" class="text-marine text-xl">Confirmez votre mot de passe</label>
+                        <input type="password" placeholder="********" class="border-solid border-2 border-marine rounded-full bg-[transparent] px-4 py-2 mb-3 placeholder:text-marine">
+                        <input @click="connectAccount" type="submit" value="S'inscrire" class="button bg-marine">
+                    </div>
+                    <div class="hidden laptop:block">
+                        <h3 class="font-saotorpes text-3xl text-white text-center">Votre lampe n'est plus qu'à un fils</h3>
+                        <img src="../assets/images/victor-medio.webp" alt="" class="w-1/2 mx-auto -mt-32">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <main v-if="step" class="pt-24 mb-12">
+            <div class="nav_account">
                 <h1 class="h1">Mon compte</h1>
                 <div class="flex flex-wrap w-full justify-between laptop:w-1/3 laptop:mx-auto">
                     <a @click="step='informations'" class="relative cursor-pointer">
@@ -18,7 +52,7 @@
                     </a>
                 </div>
             </div>
-            <div class="laptop:account my-10">
+            <div class="infos_account laptop:account my-10">
                 <div class="hidden laptop:block">
                     <p class="h1 text-blue mx-16 text-5xl">Si vous pouvez le rêver, vous pouvez le faire.</p>
                 </div>
@@ -149,7 +183,8 @@ export default {
     components: { Swiper, SwiperSlide, Button, ProductCard, },
     data(){
             return{
-                step: 'inspirations',
+                step: null,
+                connection: 'signup'
             }
         },
     setup() {
@@ -165,5 +200,13 @@ export default {
             modules: [Pagination],
         };
     },
+    methods: {
+        onSubmit($event){
+            $event.preventDefault()
+        },
+        connectAccount () {
+            this.step = 'informations'
+        },
+    }
 };
 </script>
