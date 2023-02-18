b38de1ae2da18699f17d217f3dfbdba61d80fde6
Lucie <lucie.petit02@edu.univ-fcomte.fr>
2023-02-18 15:57:34 +0100

    Corrections d'affichage - chargement dynamique en fonction de la taille

--- src/components/CustomLampes.vue
+++ src/components/CustomLampes.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-row w-full">
+    <div class="flex flex-col w-full" :class="{' laptop:flex-row': step!=='1', 'laptop:flex-col': step==='1'}">
         <div class="mr-20">
             <h1 class="h1 relative laptop:text-5xl">Personnalisez <br/>votre Ciluzio</h1>
             <p class="text-marine font-bold relative">
@@ -9,90 +9,85 @@
             </p>
         </div>
         <div v-if="step==='1'" class="size grow">
-            <swiper class="swiper px-5 w-full flex flex-col justify-center items-center laptop:hidden"
-                :slides-per-view="1"
-                :space-between="30"
-                :loop="false"
-                :centered-slides="true"
-            >
+            <swiper class="swiper laptop:hidden px-5 w-full flex flex-col justify-center items-center" :slides-per-view="1" :space-between="30" :loop="false" :centered-slides="true">
                 <swiper-slide class="relative overflow-hidden w-fit">
+                    <div class="-z-10">
+                        <img src="../assets/images/product_card_small.svg" alt="">
+                        <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
+                    </div>
                     <div class="absolute top-0 left-0 h-full w-full p-5 flex flex-col justify-between">
                         <div class="flex flex-col items-end">
                             <p class="text-white font-bold text-xl">Piccolo</p>
                             <p class="text-white font-bold text-xl">490 €</p>
-                            <Button  :secondary="true" :color="'white'" :svg="true">Configurer</Button>
+                            <Button @click="selectSize('piccolo')" :secondary="true" :color="'white'" :svg="true">Configurer</Button>
                         </div>
                     </div>
+                </swiper-slide>
+                <swiper-slide class="relative overflow-hidden w-fit">
                     <div class="-z-10">
                         <img src="../assets/images/product_card_small.svg" alt="">
                         <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
                     </div>
-                </swiper-slide>
-                <swiper-slide class="relative overflow-hidden w-fit">
                     <div class="absolute top-0 left-0 h-full w-full p-5 flex flex-col justify-between">
                         <div class="flex flex-col items-end">
                             <p class="text-white font-bold text-xl">Medio</p>
                             <p class="text-white font-bold text-xl">490 €</p>
-                            <Button  :secondary="true" :color="'white'" :svg="true">Configurer</Button>
+                            <Button @click="selectSize('medio')" :secondary="true" :color="'white'" :svg="true">Configurer</Button>
                         </div>
                     </div>
+                </swiper-slide>
+                <swiper-slide class="relative overflow-hidden w-fit">
                     <div class="-z-10">
                         <img src="../assets/images/product_card_small.svg" alt="">
                         <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
                     </div>
-                </swiper-slide>
-                <swiper-slide class="relative overflow-hidden w-fit">
                     <div class="absolute top-0 left-0 h-full w-full p-5 flex flex-col justify-between">
                         <div class="flex flex-col items-end">
                             <p class="text-white font-bold text-xl">Grande</p>
                             <p class="text-white font-bold text-xl">490 €</p>
-                            <Button  :secondary="true" :color="'white'" :svg="true">Configurer</Button>
+                            <Button @click="selectSize('grande')" :secondary="true" :color="'white'" :svg="true">Configurer</Button>
                         </div>
-                    </div>
-                    <div class="-z-10">
-                        <img src="../assets/images/product_card_small.svg" alt="">
-                        <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
                     </div>
                 </swiper-slide>
             </swiper>
             <div class="hidden laptop:grid laptop:grid-cols-3 laptop:gap-10 laptop:mx-20">
                 <div class="relative overflow-hidden w-fit">
+                    <div class="-z-10">
+                        <img src="../assets/images/product_card_small.svg" alt="">
+                        <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
+                    </div>
                     <div class="absolute top-0 left-0 h-full w-full p-5 flex flex-col justify-between">
                         <div class="flex flex-col items-end">
                             <p class="text-white font-bold text-xl">Piccolo</p>
                             <p class="text-white font-bold text-xl">490 €</p>
-                            <Button  :secondary="true" :color="'white'" :svg="true">Configurer</Button>
+                            <Button @click="selectSize('piccolo')" :secondary="true" :color="'white'" :svg="true">Configurer</Button>
                         </div>
                     </div>
+                </div>
+                <div class="relative overflow-hidden w-fit">
                     <div class="-z-10">
                         <img src="../assets/images/product_card_small.svg" alt="">
                         <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
                     </div>
-                </div>
-                <div class="relative overflow-hidden w-fit">
                     <div class="absolute top-0 left-0 h-full w-full p-5 flex flex-col justify-between">
                         <div class="flex flex-col items-end">
                             <p class="text-white font-bold text-xl">Medio</p>
                             <p class="text-white font-bold text-xl">750 €</p>
-                            <Button  :secondary="true" :color="'white'" :svg="true">Configurer</Button>
+                            <Button @click="selectSize('medio')" :secondary="true" :color="'white'" :svg="true">Configurer</Button>
                         </div>
                     </div>
+                </div>
+                <div class="relative overflow-hidden w-fit">
                     <div class="-z-10">
                         <img src="../assets/images/product_card_small.svg" alt="">
                         <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
                     </div>
-                </div>
-                <div class="relative overflow-hidden w-fit">
                     <div class="absolute top-0 left-0 h-full w-full p-5 flex flex-col justify-between">
                         <div class="flex flex-col items-end">
                             <p class="text-white font-bold text-xl">Grande</p>
                             <p class="text-white font-bold text-xl">890 €</p>
-                            <Button  :secondary="true" :color="'white'" :svg="true">Configurer</Button>
+                            <Button @click="selectSize('grande')" :secondary="true" :color="'white'" :svg="true">Configurer</Button>
                         </div>
-                    </div>
-                    <div class="-z-10">
-                        <img src="../assets/images/product_card_small.svg" alt="">
-                        <img src="../assets/images/nude-piccolo.webp" alt="" class="absolute -top-48 left-0 rotate-45">
                     </div>
                 </div>
             </div>
@@ -106,7 +101,7 @@
                         :loop="true"
                     >
                         <swiper-slide v-for="(choice, index) in content.choices" :key="index" class="w-fit">
-                                <img :src="choice.images[0].image.url" alt="">
+                                <img :src="choice.images[0].image.url" :alt="'Cylindre ' + choice.name + ' pour la lampe Ciluzio'">
                                 <p class="hidden">{{ choice.name }}</p>
                         </swiper-slide>
                     </swiper>
@@ -119,8 +114,8 @@
 
         </div>
         <div class="buttons self-end relative">
-            <p v-for="data in product" :key="data.id" class="text-xl text-marine font-bold">{{ data.price }} €</p>
-            <Button @click="step = '3'" v-if="step==='2'" :svg="true">Finaliser</Button>
+            <p v-if="step!=='1'" class="text-xl text-marine font-bold"><span v-for="data in product" :key="data.id">{{ data.price }} €</span></p>
+            <Button @click="activeProduct()" v-if="step==='2'" :svg="true">Finaliser</Button>
             <Button @click="step = '1'" v-if="step==='2'" :secondary="true">Précédent</Button>
             <Button @click="addToCart" v-if="step==='3'" :svg="true">Acheter</Button>
             <Button @click="step = '2'" v-if="step==='3'" :secondary="true">Précédent</Button>
@@ -141,11 +136,12 @@ export default{
     components: { Swiper, SwiperSlide, Button, },
     data(){
         return{
-            step: '2',
+            step: '1',
             active: false,
-            product: {},
+            product: [],
             configurator: [],
             customProduct: {},
+            slug: '',
         }
     },
     setup() {
@@ -160,29 +156,38 @@ export default{
             onSlideChange,
         };
     },
-    async mounted() {
-        await this.getProductData();
-    },
     async beforeRouteUpdate(to, from) {
         await this.getProductData(to.params.product);
     },
     computed: {
-        sortedConfigurator(){
-            return this.configurator.sort((a, b) => { 
-                return b.layerId - a.layerId 
-            })
-        }
-    },
-    methods:{
         async getProductData () {
-            const response = await client.get('wc/v3/products?slug=piccolo')
+            this.step = '2';
+            const response = await client.get('wc/v3/products?slug=' + this.slug)
             this.product = response.data;
             const productContent = this.product[0]
             this.configurator = productContent.meta_data[10].value
         },
-        addToCart(){
-            this.$store.commit('add', { product:this.product, quantity:1 })
+        sortedConfigurator(){
+            return this.configurator.sort((a, b) => { 
+                return b.layerId - a.layerId 
+            })
         },
+    },
+    methods:{
+        selectSize(slug){
+            this.step = '2'
+            this.slug = slug
+        },
+        activeProduct(){
+            this.step = '3'
+            console.log(this.product)
+            this.customProduct = this.product[0]
+
+        },
+        addToCart(){
+            this.$store.commit('add', { product:this.customProduct, quantity:1 })
+        },
+        
     },
 }
 </script>
